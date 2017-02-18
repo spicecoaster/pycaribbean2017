6360b627851297e752421362ab42133a3775822c
George Koodarappally <gjkood@yahoo.com>
2017-02-18 13:50:20 -0400

    Added networking to connect to MQTT_BROKER

--- Workshop/sample_mqtt_pub.py
+++ Workshop/sample_mqtt_pub.py
@@ -1,12 +1,26 @@
+import network
 from umqtt.simple import MQTTClient
 import time
 
 # Test message subscription using mosquitto_sub
 # mosquitto_sub -h <MQTT_BORKER_IP> -m "sensors/1/temperature"
 
-MQTT_BROKER_IP="192.168.0.5"
+MQTT_BROKER_IP="192.168.0.100"
 STATION_NO=1
 SENSOR_ID="TEMP_1"
+
+SSID='PYCARIB_IOT_WORKSHOP'
+PASSWORD='WELCOMETOIOT'
+
+def setup_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(SSID, PASSWORD)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
 
 def setup_mqtt_publisher():
     global mqtt_publisher
@@ -21,6 +35,7 @@ def publish_temp(pTemp):
     mqtt_publisher.publish(b"sensors/1/temperature", output_str.encode('ascii'))
 
 def main():
+    setup_wifi()
     setup_mqtt_publisher()
     for i in range(10):
         publish_temp(86)
