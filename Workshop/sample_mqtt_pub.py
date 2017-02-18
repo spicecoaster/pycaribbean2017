b209811b0d12556f21fff0ab7bec16c6d4911431
George Koodarappally <gjkood@yahoo.com>
2017-02-18 12:21:43 -0400

    Modified to add main() with range loop to call publish_temp

--- Workshop/sample_mqtt_pub.py
+++ Workshop/sample_mqtt_pub.py
@@ -1,4 +1,8 @@
 from umqtt.simple import MQTTClient
+import time
+
+# Test message subscription using mosquitto_sub
+# mosquitto_sub -h <MQTT_BORKER_IP> -m "sensors/1/temperature"
 
 MQTT_BROKER_IP="192.168.0.5"
 STATION_NO=1
@@ -11,5 +15,17 @@ def setup_mqtt_publisher():
     mqtt_publisher.connect()
 
 def publish_temp(pTemp):
-    output_str = "{Station: %d, Sensor: %s, Temp: %d}" % (STATION_NO, SENSOR_ID, pTemp)
-    mqtt_publisher.publish(b"sensors/temperature", output_str.encode('ascii'))
+    publish_time = "00:00:00"
+    #publish_time = time.localtime()
+    output_str = "{Time: %s, Station: %d, Sensor: %s, Temp: %d}" % (publish_time, STATION_NO, SENSOR_ID, pTemp)
+    mqtt_publisher.publish(b"sensors/1/temperature", output_str.encode('ascii'))
+
+def main():
+    setup_mqtt_publisher()
+    for i in range(10):
+        publish_temp(86)
+        time.sleep(1)
+    pass
+
+if __name__ == "__main__":
+    main()
