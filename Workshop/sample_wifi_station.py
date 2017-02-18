9995a808ab366f32351783e805858a83d8d84927
George Koodarappally <gjkood@yahoo.com>
2017-02-18 11:42:31 -0400

    Added sample code to connect to WiFi as a Station

---
+++ Workshop/sample_wifi_station.py
@@ -0,0 +1,14 @@
+import network
+
+SSID='PYCARIB_IOT_WORKSHOP'
+PASSWORD='WELCOMETOIOT'
+
+def setup_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(SSID, PASSWORD)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
